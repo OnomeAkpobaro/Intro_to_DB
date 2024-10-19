bc89e82b201b491342d7b3f2243be6f4a379c489
Onome Akpobaro <overdoseofgodsblessings@gmail.com>
2024-10-19 17:41:15 +0100

    Updated

--- task_6.sql
+++ task_6.sql
@@ -6,4 +6,4 @@ INSERT INTO customer (customer_id , customer_name , email, address)
 VALUES(3,'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
 
 INSERT INTO customer (customer_id , customer_name , email, address) 
-VALUES(4,'Nehmial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+VALUES(4,'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
